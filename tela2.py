9813107e017b23d0567cc8b45cf2c9cbec1db76a
Augusto <augustovmagro@gmail.com>
2023-06-12 00:39:09 -0300

    Alteração cor

--- tela2.py
+++ tela2.py
@@ -4,5 +4,5 @@ import customtkinter as ck
 
 class Tela2(ck.CTkFrame):
     def __init__(self, parent):
-        super().__init__(master=parent, fg_color="blue")
+        super().__init__(master=parent, fg_color="Pink")
         self.grid(row=0, column=1, sticky="nsew")
